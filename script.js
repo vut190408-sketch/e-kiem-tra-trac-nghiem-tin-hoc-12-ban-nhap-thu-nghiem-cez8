3c5ee96c2bb56312910414f92a5f39b298e17de9
vut190408-sketch <vut190408@gmail.com>
2026-08-05 12:35:56 +0700

    update script.js

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ let QUIZ_DATA_SETS = {};
 
         
     const backgroundMusic = null;
-    const correctAnswerSound = null;
-    const incorrectAnswerSound = null;
+    const correctAnswerSound = new Audio('correct.mp3');
+    const incorrectAnswerSound = new Audio('incorrect.mp3');
   
         const ICONS = {
             volumeOn: '<svg viewBox="0 0 24 24"><path d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"></path></svg>',
@@ -1036,7 +1036,7 @@ let QUIZ_DATA_SETS = {};
         // =========================================================================
         // === HỆ THỐNG KIỂM TRA PHIÊN BẢN TỰ ĐỘNG (AUTO-UPDATE) ===
         // =========================================================================
-        const CURRENT_VERSION = 1785638452135;
+        const CURRENT_VERSION = 1785908147062;
         const CHECK_INTERVAL = 3 * 60 * 1000;
 
         async function checkUpdate() {
